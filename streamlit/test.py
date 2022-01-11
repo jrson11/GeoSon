b7c298f4cb8a4cf9d78b1a1862ec31f7f4a6a138
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-11 12:30:08 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -2,14 +2,12 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
-import altair as alt
-from vega_datasets import data
+
 
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
     
-    counties = alt.topo_feature(data.us_10m.url, 'counties')
-    source = data.unemployment.url
+
     
