206d4ddeaedfbfdd2ec207943b1fa4714118fe4b
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-11 12:29:30 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -6,10 +6,6 @@ import altair as alt
 from vega_datasets import data
 
 
-
-
-
-
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -17,5 +13,9 @@ with st.echo(code_location='below'):
     counties = alt.topo_feature(data.us_10m.url, 'counties')
     source = data.unemployment.url
     
-    st.altair_chart(alt.Chart(counties).mark_geoshape().encode(color='rate:Q').transform_lookup(lookup='id',from_=alt.LookupData(source, 'id', ['rate'])).project(
-    type='albersUsa').properties(width=500,height=300)
+    st.altair_chart(alt.Chart(counties)
+        .mark_geoshape()
+        .encode(color='rate:Q')
+        .transform_lookup(lookup='id',from_=alt.LookupData(source, 'id', ['rate']))
+        .project(type='albersUsa')
+        .properties(width=500,height=300))
