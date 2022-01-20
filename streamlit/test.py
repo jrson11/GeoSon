40f6a701ece7c07c340254ff432c0d38fabdbb86
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-20 17:12:45 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 
 
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
