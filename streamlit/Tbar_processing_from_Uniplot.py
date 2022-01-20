9dfacd9387950753195deabd449bb9581c5a88f7
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-20 17:30:45 -0600

    Create Tbar_processing_from_Uniplot.py

---
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+
+
+# -- Create sidebar for plot controls
+st.sidebar.markdown('## Set Nt Parameters')
+Nt_und = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Nt_rem = st.sidebar.slider('Nt remolded', 5.0, 15.0, 10.5)  # min, max, default
+
+
+# -- Create upbar to import data
+uploaded_files = st.file_uploader("Choose ASC files", accept_multiple_files=True)
+
+
+    
