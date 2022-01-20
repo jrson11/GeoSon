9cb29d41564e7a4f01b0567b0aec7a606bc2f3a7
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-20 17:18:08 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -11,6 +11,12 @@ add_selectbox = st.sidebar.selectbox(
 
 uploaded_files = st.file_uploader("Choose ASC files", accept_multiple_files=True)
 
+# -- Create sidebar for plot controls
+st.sidebar.markdown('## Set Plot Parameters')
+dtboth = st.sidebar.slider('Time Range (seconds)', 0.1, 8.0, 1.0)  # min, max, default
+dt = dtboth / 2.0
+
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
