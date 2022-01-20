89c8b4934787b92c8ac1f205acb88b4e509e83e2
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-20 17:24:13 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -9,17 +9,9 @@ import pandas as pd
 st.sidebar.markdown('## Set Nt for Undisturbed Sample')
 Nt_und = st.sidebar.slider('Nt [-]', 5, 15.0, 10.5)  # min, max, default
 
-'''
-
 # -- Create sidebar to define input parameter
 st.sidebar.markdown('## Set Nt for Remolded Sample')
 Nt_rem = st.sidebar.slider('Nt [-]', 5, 15.0, 10.5)  # min, max, default
-
-# -- Create sidebar to choose user
-add_selectbox = st.sidebar.selectbox(
-    "Fugro User?",
-    ("J", "B", "S")
-)
 
 # -- Create upbar to import data
 uploaded_files = st.file_uploader("Choose ASC files", accept_multiple_files=True)
@@ -29,5 +21,3 @@ with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
     
-
-'''
