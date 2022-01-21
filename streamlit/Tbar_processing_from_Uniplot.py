b108573b548d3002ba8b15fc6991ca08076b3f79
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-21 10:51:08 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -11,6 +11,18 @@ Nt_rem = st.sidebar.slider('Nt remolded', 5.0, 15.0, 10.5)  # min, max, default
 asc_list = st.file_uploader("Choose ASC files", accept_multiple_files=True)
 n_asc = len(asc_list)
 
-print(n_asc)
+# Processing
+
+def dfTbarfromUniplotASC():
+    
+    df_TBAR_first = pd.DataFrame()
+    df_TBAR_last = pd.DataFrame()
+    
+    for i in range(n_asc):
+        txt_file = asc_list[i]
+        loca = txt_file.split()[0]
+        print(txt_file)
+        
+
 
     
