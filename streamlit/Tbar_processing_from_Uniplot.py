d32cdca3e8ccde4a6a8e3ce070fe084b3b3d5f16
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-24 14:58:42 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -6,7 +6,7 @@ import streamlit as st
 # FUSAMI_ENG - Tbar processing
 Purpose: To extract the first & last Tbar push data from cycles in Uniplot file \n
 Author: J.Son, Ph.D., P.E. \n
-Last Update: 1/22/2021 \n
+Last Update: 1/24/2021 \n
 Note: I am so sorry for my bad coding script \n
 '''
 
