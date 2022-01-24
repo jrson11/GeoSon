6f16e016e5e830411f6a9e8e7b449b2c44fca3d9
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-24 10:16:27 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -9,7 +9,7 @@ Purpose: To extract the first & last Tbar push data from cycles in Uniplot file 
 Author: J.Son, Ph.D., P.E. \n
 Last Update: 1/22/2021 \n
 '''
-
+'''
 # -- Create sidebar for plot controls
 st.sidebar.markdown('## Set Nt Parameters')
 Nt_und = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
@@ -20,3 +20,4 @@ asc_files = st.sidebar.file_uploader('Choose converted CSV files from Uniplot AS
 n_asc = len(asc_files)
 #
 st.text('Number of ASC files: '+str(n_asc))
+'''
