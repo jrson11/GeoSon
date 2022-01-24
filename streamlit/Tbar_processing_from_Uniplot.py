44e715c92463fcaf44372380a237fd3f6b9591bc
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-24 10:15:48 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -1,6 +1,14 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+'''
+# Geotechnics - Tbar processing
+Purpose: To extract the first & last Tbar push data from cycles in Uniplot file \n
+Author: J.Son, Ph.D., P.E. \n
+Last Update: 1/22/2021 \n
+'''
 
 # -- Create sidebar for plot controls
 st.sidebar.markdown('## Set Nt Parameters')
@@ -8,8 +16,7 @@ Nt_und = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, defau
 Nt_rem = st.sidebar.slider('Nt remolded', 5.0, 15.0, 10.5)  # min, max, default
 
 # -- Create upbar to import data
-asc_list = st.file_uploader("Choose ASC files", accept_multiple_files=True)
-n_asc = len(asc_list)
-
-
-    
+asc_files = st.sidebar.file_uploader('Choose converted CSV files from Uniplot ASC', accept_multiple_files=True)
+n_asc = len(asc_files)
+#
+st.text('Number of ASC files: '+str(n_asc))
