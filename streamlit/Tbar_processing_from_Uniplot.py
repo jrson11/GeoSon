1d99d50177ebdf82012dac151c4a360f266162eb
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-24 10:24:31 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -1,11 +1,4 @@
 import streamlit as st
-
-import matplotlib.pyplot as plt
-
+import pandas as pd
 import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-
-plt.hist(arr, bins=20)
-
-st.pyplot()
+import matplotlib.pyplot as 
