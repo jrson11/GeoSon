fe1f60492dcb34580c443063298837ca50fd0c40
J.Son <91623332+jrson11@users.noreply.github.com>
2022-01-24 14:58:17 -0600

    Update Tbar_processing_from_Uniplot.py

--- streamlit/Tbar_processing_from_Uniplot.py
+++ streamlit/Tbar_processing_from_Uniplot.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
 import streamlit as st
 
 '''
-# Geotechnics - Tbar processing
+# FUSAMI_ENG - Tbar processing
 Purpose: To extract the first & last Tbar push data from cycles in Uniplot file \n
 Author: J.Son, Ph.D., P.E. \n
 Last Update: 1/22/2021 \n
