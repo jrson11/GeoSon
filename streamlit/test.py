43131fcbf4b5b4cf25b446c50d914f540b5bb486
J.Son <91623332+jrson11@users.noreply.github.com>
2022-02-07 11:37:56 -0600

    Update test.py

--- streamlit/test.py
+++ streamlit/test.py
@@ -2,3 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.markdown('## Hi')
+
+
+st.header("test html import")
+
+HtmlFile = open("google.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code)
