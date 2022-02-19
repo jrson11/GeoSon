a5825db3e931b426d4ef414d9f03f7db6c307d21
J.Son <91623332+jrson11@users.noreply.github.com>
2022-02-19 17:34:22 -0600

    Update ez_CcCrCs.py

--- streamlit/ez_CcCrCs.py
+++ streamlit/ez_CcCrCs.py
@@ -6,18 +6,18 @@ import streamlit as st
 st.markdown('## ez_Cc Cr Cs from log(x)-y plot')
 
 # Sidebar
-st.sidebar.markdown('## Tangent Lines')
-Cc_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cc_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cc_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+st.sidebar.markdown('## Tangent Lines: y = ax + b')
+Cc_point_1_index = st.sidebar.slider('Index of starting Cc point', 5.0, 15.0, 10.5) 
+Cc_point_2_slope = st.sidebar.slider('Slope(a) of ending Cc line', 5.0, 15.0, 10.5)  
+Cc_point_2_intercept = st.sidebar.slider('Intercept(b) of ending Cc line', 5.0, 15.0, 10.5) 
 #
-Cr_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cr_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cr_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cr_point_1_index = st.sidebar.slider('Index of starting Cr point', 5.0, 15.0, 10.5) 
+Cr_point_2_slope = st.sidebar.slider('Slope(a) of ending Cr line', 5.0, 15.0, 10.5)  
+Cr_point_2_intercept = st.sidebar.slider('Intercept(b) of ending Cr line', 5.0, 15.0, 10.5)
 #
-Cs_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cs_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-Cs_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cs_point_1_index = st.sidebar.slider('Index of starting Cs point', 5.0, 15.0, 10.5) 
+Cs_point_2_slope = st.sidebar.slider('Slope(a) of ending Cs line', 5.0, 15.0, 10.5)  
+Cs_point_2_intercept = st.sidebar.slider('Intercept(b) of ending Cs line', 5.0, 15.0, 10.5)
 
 x = np.linspace(1,10,1)
 y = x*2
