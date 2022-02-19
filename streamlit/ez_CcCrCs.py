4e25423f5a0a713dd035abd108fce75c7eab4864
J.Son <91623332+jrson11@users.noreply.github.com>
2022-02-19 17:32:05 -0600

    Update ez_CcCrCs.py

--- streamlit/ez_CcCrCs.py
+++ streamlit/ez_CcCrCs.py
@@ -7,10 +7,17 @@ st.markdown('## ez_Cc Cr Cs from log(x)-y plot')
 
 # Sidebar
 st.sidebar.markdown('## Tangent Lines')
-Cc_point_1_index = = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cc_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
 Cc_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
 Cc_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
-
+#
+Cr_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cr_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cr_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+#
+Cs_point_1_index = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cs_point_2_slope = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
+Cs_point_2_intercept = st.sidebar.slider('Nt undisturbed', 5.0, 15.0, 10.5)  # min, max, default
 
 x = np.linspace(1,10,1)
 y = x*2
