8ed6c4717307d0d6fa26b33ef0b387a54da99820
J.Son <91623332+jrson11@users.noreply.github.com>
2022-02-20 10:15:10 -0600

    Update ez_CcCrCs.py

--- streamlit/ez_CcCrCs.py
+++ streamlit/ez_CcCrCs.py
@@ -26,6 +26,7 @@ Cs_point_2_slope = st.sidebar.slider('Slope(a) of ending Cs line', 5.0, 15.0, 10
 Cs_point_2_intercept = st.sidebar.slider('Intercept(b) of ending Cs line', 5.0, 15.0, 10.5)
 
 # Data
-#df = pd.read_csv('./data-CcCrCs.csv')
+garret_burhenn_pitches_csv = Path(__file__).parents[1] / 'data-CcCrCs.csv'
+
 #
 #st.dataframe(df)
