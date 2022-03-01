6754039bb270152a4b6ab65478926f1a8d5eedf1
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 12:27:32 -0600

    Update secret.py

--- streamlit/secret.py
+++ streamlit/secret.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
-st.markdown('# Test page to keep secret')
+st.markdown("# Test page to keep secret")
 
-username = st.sidebar.text_input('Username', 'username')
 password = st.sidebar.text_input('Password', 'password')
 
-
-st.write('The current movie title is', title)
 
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
