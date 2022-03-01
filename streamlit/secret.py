8b69274e4d8a24e8dddd3e0509c805990b0ef4d1
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 12:24:43 -0600

    Update secret.py

--- streamlit/secret.py
+++ streamlit/secret.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.write("DB username:", st.secrets["db_username"])
+
+title = st.sidebar.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+
+#st.write("DB username:", st.secrets["db_username"])
 
