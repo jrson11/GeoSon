db8b059f353c863c55145f35fb6be3a45f25a8af
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 12:32:50 -0600

    Update secret.py

--- streamlit/secret.py
+++ streamlit/secret.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
+# Initialization ----------------------------------------
 st.markdown("# Test page to keep secret")
-
 password = st.sidebar.text_input('Password?', 'password')
 
+# Main --------------------------------------------------
+def main():
+  st.write('Assa')
+
+# Membership --------------------------------------------
 if password == st.secrets["db_password"]:
   st.markdown('## Welcome to KUPEA')
   main()
@@ -12,5 +17,4 @@ else:
   st.markdown('## Please join KUPEA')
 
 
-def main():
-  st.write('Assa')
+
